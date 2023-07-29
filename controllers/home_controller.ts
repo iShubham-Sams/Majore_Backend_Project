c0e7be3ff586ec8886bcee8c545cbecf02cb956b
iShubham-Sams <shubhambhandari12345hb@gmail.com>
2023-07-29 12:35:50 +0530

    get post done

--- controllers/home_controller.ts
+++ controllers/home_controller.ts
@@ -1,6 +1,17 @@
 import { Response, Request } from "express";
+import { Post } from "../models/post";
 
 export const homeController = (req: Request, res: Response) => {
-  res.cookie("user_id", 25);
-  return res.end("<h1>Express is up for the codial</h1>");
+  // res.cookie("user_id", 25);
+  // return res.end("<h1>Express is up for the codial</h1>");
+
+  Post.find({})
+    .populate("user")
+    .exec()
+    .then((data) => {
+      return res.send(data);
+    })
+    .catch((err) => {
+      return res.send("Error while finding Post");
+    });
 };
